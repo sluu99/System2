815f47d702c0e422efcde29611256ea8f261c849
Steven Luu <steven@luu.io>
2016-04-02 18:25:28 -0700

    Initial commit

---
+++ InvariantString.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+public static class InvariantString
+{
+    public static string Format(string format, params object[] parameters)
+    {
+        return string.Format(CultureInfo.InvariantCulture, format, parameters);
+    }
+}
---
+++ SystemTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class SystemTime
+{
+    private static Func<DateTimeOffset> NowFunc = () => DateTimeOffset.Now;
+
+    public static DateTimeOffset UtcNow
+    {
+        get
+        {
+            return NowFunc().ToUniversalTime();
+        }
+    }
+
+    public static DateTimeOffset Now
+    {
+        get
+        {
+            return NowFunc();
+        }
+    }
+
+    public static void SetNowFunc(Func<DateTimeOffset> func)
+    {
+        if (func == null)
+        {
+            throw new ArgumentNullException(nameof(func));
+        }
+
+        NowFunc = func;
+    }
+}
